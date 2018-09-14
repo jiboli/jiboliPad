1c8acdea1ceb5ee1f211d87a457352b2ee328dc6
Jiboli <joao.riboli@outlook.com>
2018-09-14 03:45:57 -0300

    PWA deploy

---
+++ sw.js
@@ -0,0 +1,40 @@
+const version = "0.2.33";
+const cacheName = `jibolipad-${version}`;
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `/`,
+        `/index.html`,
+        `/styles/inline.css`,
+        `/scripts/app.js`,
+        `/scripts/clipboard.min.js`,
+        `/scripts/lz-string.js`,
+        `/images/icons/icon-32x32.png`,
+        `/images/svg/file.svg`,
+        `/images/svg/external-link-symbol.svg`,
+        `/images/svg/link-symbol.svg`,
+        `/images/svg/padlock.svg`,
+        `/images/svg/padlock-unlock.svg`,
+        `/images/svg/paste-from-clipboard.svg`,
+        `/images/svg/save-file-option.svg`,
+        `/images/svg/share-symbol.svg`
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
