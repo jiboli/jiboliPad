415bbd3817a1f3f9097d2dc24b3531f2e5f0f120
Jiboli <joao.riboli@outlook.com>
2018-09-15 11:26:42 -0300

    sasd

--- scripts/app.js
+++ scripts/app.js
@@ -166,6 +166,24 @@ if(content) {
     
 
   });
+
+  window.addEventListener('beforeinstallprompt', function(e) {
+    // beforeinstallprompt Event fired
+  
+    // e.userChoice will return a Promise.
+    // For more details read: https://developers.google.com/web/fundamentals/getting-started/primers/promises
+    e.userChoice.then(function(choiceResult) {
+  
+      console.log(choiceResult.outcome);
+  
+      if(choiceResult.outcome == 'dismissed') {
+        console.log('User cancelled home screen install');
+      }
+      else {
+        console.log('User added to home screen');
+      }
+    });
+  });
   /*document.getElementById('btnShare').addEventListener('click', function(){
     app.toggleNewDialog(false);
     
